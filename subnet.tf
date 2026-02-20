6c32c5fb9f44340d7ff5a0048e77fe81dc27331c
srinivasyadav <gaddamsrinu337@gmail.com>
2026-02-20 22:02:17 +0530

    subnet code

---
+++ subnet.tf
@@ -0,0 +1,7 @@
+# Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "my-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
